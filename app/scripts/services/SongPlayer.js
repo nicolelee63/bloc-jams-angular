2373d548d8285b4c88c3294e479575a97bd59aaa
nicolelee63 <nicole.lee633@gmail.com>
2017-04-08 16:21:00 +0800

    checkpoint 8 completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,8 +1,19 @@
 (function() {
-     function SongPlayer() {
+     function SongPlayer(Fixtures) {
         var SongPlayer = {};
          
-        var currentSong = null;
+        var currentAlbum = Fixtures.getAlbum();
+         
+        var getSongIndex = function(song) {
+            return currentAlbum.songs.indexOf(song);
+        };
+         
+        /**
+        * @desc Active song object from list of songs
+        * @type {Object}
+        */
+         
+        SongPlayer.currentSong = null;
         
         /**
         * @desc Buzz object audio file
@@ -29,7 +40,7 @@
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
-                currentSong.playing = null;
+                SongPlayer.currentSong.playing = null;
             }
  
         currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -37,14 +48,15 @@
             preload: true
         });
  
-        currentSong = song;
+        SongPlayer.currentSong = song;
         };
          
     SongPlayer.play = function(song) {
-        if (currentSong !== song) {
+        song = song || SongPlayer.currentSong;
+        if (SongPlayer.currentSong !== song) {
             setSong(song);
             playSong();
-        } else if(currentSong === song) {
+        } else if(SongPlayer.currentSong === song) {
             if (currentBuzzObject.isPaused()) {
                 currentBuzzObject.play();
             }
@@ -52,8 +64,23 @@
     }; 
     
     SongPlayer.pause = function(song) {
-     currentBuzzObject.pause();
-     song.playing = false;
+        song = song || SongPlayer.currentSong;
+        currentBuzzObject.pause();
+        song.playing = false;
+    };
+    
+    SongPlayer.previous = function() {
+        var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+        currentSongIndex--;
+        
+        if (currentSongIndex < 0) {
+            currentBuzzObject.stop();
+            SongPlayer.currentSong.playing = null;
+        } else {
+            var song = currentAlbum.songs[currentSongIndex];
+            setSong(song);
+            playSong(song);
+        }
     };
          
     return SongPlayer;
@@ -61,5 +88,5 @@
  
      angular
          .module('blocJams')
-         .factory('SongPlayer', SongPlayer);
+         .factory('SongPlayer', ['Fixtures'SongPlayer]);
  })();
