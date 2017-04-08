bba9de2af45dccdd205dcb165ea42a2540448646
nicolelee63 <nicole.lee633@gmail.com>
2017-04-08 15:32:30 +0800

    assignment 6 completed

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -36,6 +36,13 @@
          return albumPicasso;
         };
          
+        Fixtures.getCollection = function(numberOfAlbums){
+            this.albums = [];
+            for (var i=0; i < 12; i++) {
+                this.albums.push(angular.copy(albumPicasso));
+            }
+        };
+         
         return Fixtures;
      }
  
